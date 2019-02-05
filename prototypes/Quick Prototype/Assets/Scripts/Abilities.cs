aafccf6d5ab7aad89f4c2643a02083e166635fd4
zkyenet <zwonglema@stanford.edu>
2019-02-05 15:05:05 -0800

    Max Velocity (buggy?), add ability 1(throw lure), max jumps

---
+++ prototypes/Quick Prototype/Assets/Scripts/Abilities.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Abilities : MonoBehaviour {
+
+    public GameObject lure;
+    public int throw_speed;
+    public Camera cam;
+
+    private Transform me_transform;
+    private GameObject spawned_lure;
+
+    public void SpawnLure()
+    {
+        spawned_lure = Instantiate(lure, me_transform.position + new Vector3(0,1,0), lure.transform.rotation);
+        spawned_lure.GetComponent<Rigidbody>().velocity += (cam.transform.forward * throw_speed) + new Vector3(0,1*throw_speed,0);
+
+    }
+
+    // Use this for initialization
+    void Start () {
+        me_transform = this.gameObject.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
