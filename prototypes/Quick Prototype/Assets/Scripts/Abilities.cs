3650359bbc3c2f374f53582c63e95876848f9c63
Rabbit Hole <admin@rabbitholevr.org>
2019-02-28 19:56:16 -0800

    Changed lures to be glowing spheres
    Want to still make them expand while cast, like firballs, and become dull and transparent as lifetime runs out

--- prototypes/Quick Prototype/Assets/Scripts/Abilities.cs
+++ prototypes/Quick Prototype/Assets/Scripts/Abilities.cs
@@ -40,8 +40,8 @@ public class Abilities : MonoBehaviour {
     {
         if (hsm.mana >= 2) {
             hsm.mana -= 2;
-            spawned_lure = Instantiate(lure, me_transform.position + new Vector3(0, 1, 0), lure.transform.rotation);
-            spawned_lure.GetComponent<Rigidbody>().velocity += (cam.transform.forward * throw_speed) + new Vector3(0, 1 * throw_speed, 0);
+            spawned_lure = Instantiate(lure, me_transform.position + new Vector3(0, 13, 0), lure.transform.rotation);
+            //spawned_lure.GetComponent<Rigidbody>().velocity += (cam.transform.forward * throw_speed) + new Vector3(0, 1 * throw_speed, 0);
 
             hsm.lureDict[nextIndex] = spawned_lure;
             spawned_lure.GetComponent<LureScript>().index = nextIndex;
